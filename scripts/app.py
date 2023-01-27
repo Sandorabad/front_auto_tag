dd9f8604cc174cd462b6a4dc5b11d8c92cc234d6
Sandor Abad <sandorabad.cl@gmail.com>
2023-01-26 22:20:32 -0300

    Updates_8

--- scripts/app.py
+++ scripts/app.py
@@ -4,11 +4,11 @@ from PIL import Image
 import pandas as pd
 import io
 
+st.set_page_config(page_title="Retail Auto Tagging", page_icon=":guardsman:", layout="wide")
 
-#page_config
-st.set_page_config(page_title="Image Prediction App", page_icon=":guardsman:", layout="wide")
+# logo_path = "path/to/logo.png"
+# st.image(logo_path, width=150)
 
-# Load the model from GCS
 def predict_image(image):
     # Add code to send the image to the API and receive a prediction
     api_url = 'http://127.0.0.1:8002/pred/'
@@ -18,26 +18,31 @@ def predict_image(image):
 def main():
     st.title("Upload an image for prediction")
     image_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"], accept_multiple_files=True)
-
     if image_file is not None:
-        categorias = {}
+        lista_vacia = []
         for img in image_file:
-            image_name = img.name # esto se encuentra en el objeto de uploded file
-            image_category = predict_image(img).json()
-            categorias[image_name] = image_category #añadimos una entrada en el diccionario
 
-    excel = pd.DataFrame(categorias)
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-        excel.to_excel(writer, sheet_name='Sheet1')
-        writer.save()
 
-    st.download_button(
-    label="Descagar Excel",
-    data=buffer,
-    file_name="clasificacion.xlsx",
-    mime="application/vnd.ms-excel"
-    )
+            result = predict_image(img).json()
+            result["name_img"] = img.name
+            st.success(f"Procesando imagen {img.name}")
+            lista_vacia.append(result)
+
+        excel = pd.DataFrame(lista_vacia)
+
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            excel.to_excel(writer, sheet_name='Sheet1')
+            writer.save()
+
+        if excel is not None:
+            st.download_button(
+            label="Descagar Excel",
+            data=buffer,
+            file_name="clasificacion.xlsx",
+            mime="application/vnd.ms-excel"
+            )
+
 
 if __name__ == '__main__':
     main()
